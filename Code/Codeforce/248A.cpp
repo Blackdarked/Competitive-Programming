59f765d1a1d69325ab1ae0ed913d9afb33b83f82
Blackdark <Thieudinhnamtai@gmail.com>
2021-03-26 22:04:38 +0700

    Add files for archive

--- Code/Codeforce/248A.cpp
+++ Code/Codeforce/248A.cpp
@@ -6,5 +6,15 @@ int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);
 
-	return 0;
+    int n, l(0), r(0), left(0), right(0);
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> l >> r;
+		left += l;
+		right += r;
+	}
+    left = min(left, n - left);
+    right = min(right, n - right);
+    cout << left + right << endl;
+    return 0;
 }
