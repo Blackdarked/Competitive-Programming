b4c1c7ff519e33219334790703af9f67a5e1914c
Blackdark <Thieudinhnamtai@gmail.com>
2021-03-30 18:13:44 +0700

    Add files for archive

---
+++ PrepExam/UPGRADE.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	freopen("UPGRADE.INP", "r", stdin);
+	freopen("UPGRADE.OUT", "w", stdout);
+
+    int N, M, u, v, t;
+    
+    return 0;
+}
---
+++ PrepExam/mytemplate2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        
+    }
+	return 0;
+}
