f615af2d561b657af56310a337f16e0dcb46409a
Blackdark <thieudinhnamtai@gmail.com>
2021-04-09 22:52:05 +0700

    Add files for archive

--- Template/CodeforcesTemplate.cpp
+++ Template/CodeforcesTemplate.cpp
@@ -12,10 +12,24 @@ typedef vector<pi> vii;
 #define MP make_pair;
 #define REP(i, a, b) for (int i = a; i <= b; i++)
 
+void Try(int n) {
+	int A[100000];
+	for (int i = 0; i <= 1; i++) {
+		A[i] = n;
+		if (i == n) 
+			for (int j = 0; j < n; j++) 
+				cout << A[i];
+		else {
+			Try(i++);
+		}
+	}
+}
+
 int main() {
 	cin.tie(0);
 	cout.tie(0);
 	ios_base::sync_with_stdio(0);
+
 
 	return 0;
 }
